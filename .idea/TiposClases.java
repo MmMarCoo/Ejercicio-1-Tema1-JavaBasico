76716cb04f143b1503f8a1a4fad1bbd634a77292
Marco Rodrigues <marcofernandes1997@gmail.com>
2022-01-25 18:11:31 +0000

    Create TiposClases.java

---
+++ .idea/TiposClases.java
@@ -0,0 +1,52 @@
+package TemaUno;
+
+public class TiposClases {
+
+    public static void main(String[] args) {
+
+        //enteros
+        byte number1 = 1 ;
+
+            System.out.println(number1);
+
+        short number2 = 2 ;
+
+            System.out.println(number2);
+
+        int number3 = 3 ;
+
+            System.out.println(number3);
+
+        long number4 = 4 ;
+
+            System.out.println(number4);
+
+        // decimales
+        float decimal1 = 6.3f ;
+
+            System.out.println(decimal1);
+
+        double decimal2 = 7.11d ;
+
+            System.out.println(decimal2);
+
+        // boolean
+        boolean verdadero = true ;
+
+            System.out.println(verdadero);
+
+        boolean falso = false ;
+
+            System.out.println(falso);
+
+        // texto
+        String nombre = "Marco" ;
+
+            System.out.println(nombre);
+
+        char character1 = 'a' ;
+
+            System.out.println(character1);
+
+    }
+}
